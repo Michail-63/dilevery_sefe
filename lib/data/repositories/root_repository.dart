931bc5bce0d3ac49726914144f67e5abcc2f29fd
michail <chester_87_06@mail.ru>
2024-06-06 21:43:23 +0300

    before marge

--- lib/data/repositories/root_repository.dart
+++ lib/data/repositories/root_repository.dart
@@ -4,11 +4,28 @@ import 'package:delivery/data/models/lists/list_categories.dart';
 
 class RootRepository {
 
-  //
-  // Future <List<Category<List<Dish>>>> getDish(name) async {
-  //   await Future.delayed(const Duration(seconds: 2));
-  //   return name;
-  // }
+
+
+  Future<List<Dish>> getDish() async {
+    await Future.delayed(Duration(seconds: 2));
+    final categoriesList = categories as Map<String, dynamic>;
+    final category = categoriesList['Category'] as Map<String, dynamic>;
+    final dishesList = category.entries.map((e) {
+      final dishesData =
+          (e.value as Map<String, dynamic>)['Dish'] as Map<String, dynamic>;
+      final price = dishesData['price'];
+      final image = dishesData['image'];
+      final text = dishesData['text'];
+
+      return Dish(
+        title: e.key,
+        price: price,
+        image: image,
+        text: text,
+      );
+    }).toList();
+    return dishesList;
+  }
 
 
 
@@ -19,10 +36,6 @@ class RootRepository {
 
   Future<List<Category>> getCategories() async {
     await Future.delayed(Duration(seconds: 2));
-    print('dcfasdfc111111');
     return categories;
   }
-
-
-
 }
