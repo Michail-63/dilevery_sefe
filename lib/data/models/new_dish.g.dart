53c1a697eea2706ce78064dd3c20cf672e0b75f9
michail <chester_87_06@mail.ru>
2024-06-11 19:56:54 +0300

    before marge

---
+++ lib/data/models/new_dish.g.dart
@@ -0,0 +1,68 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'new_dish.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NewDishAdapter extends TypeAdapter<NewDish> {
+  @override
+  final int typeId = 2;
+
+  @override
+  NewDish read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NewDish(
+      id: fields[0] as String,
+      isRecommended: fields[1] as bool,
+      isFavorites: fields[2] as bool,
+      isTheBest: fields[3] as bool,
+      isPopular: fields[4] as bool,
+      text: fields[9] as String,
+      title: fields[5] as String,
+      price: fields[6] as int,
+      image: fields[8] as String,
+      count: fields[7] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NewDish obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.isRecommended)
+      ..writeByte(2)
+      ..write(obj.isFavorites)
+      ..writeByte(3)
+      ..write(obj.isTheBest)
+      ..writeByte(4)
+      ..write(obj.isPopular)
+      ..writeByte(5)
+      ..write(obj.title)
+      ..writeByte(6)
+      ..write(obj.price)
+      ..writeByte(7)
+      ..write(obj.count)
+      ..writeByte(8)
+      ..write(obj.image)
+      ..writeByte(9)
+      ..write(obj.text);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NewDishAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:delivery/config/theme.dart';
 import 'package:delivery/data/models/lists/list_new_dishes.dart';
 import 'package:delivery/data/models/new_dish.dart';
-import 'package:delivery/data/models/review.dart';
 import 'package:delivery/pages/main/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-
-
 
 void main() async {
   await Hive.initFlutter();
 
   // Hive.registerAdapter(ReviewAdapter());
-  Hive.registerAdapter( NewDishAdapter());
+  Hive.registerAdapter(NewDishAdapter());
   await Hive.openBox<NewDish>('new_dish_box');
-  if (Hive.box<NewDish>('new_dish_box').isEmpty){
-    await Hive.box<NewDish>('new_dish_box').addAll(listDish) ;
+  if (Hive.box<NewDish>('new_dish_box').isEmpty) {
+    await Hive.box<NewDish>('new_dish_box').addAll(listDish);
   }
   // final reviewBox =  await  Hive.openBox<Review>("review_box");
 
-
-  runApp(const MyApp(
-
-
-
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -34,13 +25,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-    theme: lightTheme,
-        debugShowCheckedModeBanner:false,
-      home:MainPage()
+        theme: lightTheme, debugShowCheckedModeBanner: false, home: MainPage()
 
         // SplashPage(duration: 3,goToPage:WelcomePage())
-    );
+        );
   }
 }
-
