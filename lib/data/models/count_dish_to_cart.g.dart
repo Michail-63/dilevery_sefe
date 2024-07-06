b60329f824728e5e93606d89627ee1ef33fb394f
michail <chester_87_06@mail.ru>
2024-07-06 08:46:16 +0300

    new repository 3 step

---
+++ lib/data/models/count_dish_to_cart.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'count_dish_to_cart.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CountDishToCartAdapter extends TypeAdapter<CountDishToCart> {
+  @override
+  final int typeId = 3;
+
+  @override
+  CountDishToCart read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CountDishToCart(
+      dishId: fields[0] as String,
+      count: fields[1] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CountDishToCart obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.dishId)
+      ..writeByte(1)
+      ..write(obj.count);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CountDishToCartAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
